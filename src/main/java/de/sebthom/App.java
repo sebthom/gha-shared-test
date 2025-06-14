47bd37fb466ebb25aa3663cfb5606b9cb6a814c4
sebthom <sebthom@users.noreply.github.com>
2025-06-14 23:04:21 +0200

    test

---
+++ src/main/java/de/sebthom/App.java
@@ -0,0 +1,14 @@
+package de.sebthom;
+
+/**
+ * @author Sebastian Thomschke
+ */
+public class App {
+   public static void main(final String[] args) {
+      new App().run();
+   }
+
+   public void run() {
+      System.out.println("Hello World!");
+   }
+}
---
+++ src/test/java/de/sebthom/AppTest.java
@@ -0,0 +1,19 @@
+package de.sebthom;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Sebastian Thomschke
+ */
+class AppTest {
+
+   /**
+    * Rigorous Test :-)
+    */
+   @Test
+   void shouldAnswerWithTrue() {
+      assertTrue(true);
+   }
+}
